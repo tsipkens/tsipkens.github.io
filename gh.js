6ff1db0227ce2439a2ee2aca2d44ea319f7f5b1e
tsipkens <tsipkens@gmail.com>
2021-10-18 12:15:40 -0400

    Add profile image
    + Rename a few classes
    + Minor styling updates

--- gh.js
+++ gh.js
@@ -43,54 +43,38 @@ function requestUserRepos(username) {
         while (root.firstChild) {
             root.removeChild(root.firstChild);
         }
-        if (data.message === "Not Found") {
-            let ul = document.getElementById('userRepos');
+
+        // Get the ul with id of of userRepos
+        let ul = document.getElementById('userRepos');
+        
+        // Loop over each object in data array
+        for (let i in data) {
+            console.log(data[i])
+            if (!(validRepos.includes(data[i].name))) {
+                continue;
+            }
 
             // Create variable that will create li's to be added to ul
             let li = document.createElement('li');
 
             // Add Bootstrap list item class to each li
             li.classList.add('pub-entry')
-                // Create the html markup for each li
+
+            // Create the html markup for each li
+            tstar = formater('<a class="little-icon" href=' + data[i].html_url + 
+                '/stargazers><i class="far fa-star"></i>', data[i].stargazers_count, '</a>')
+            tfork = formater('<a class="little-icon" href=' + data[i].html_url + 
+                '/network/members><i class="fas fa-code-branch"></i>', data[i].forks_count, '</a>')
+            console.log(tfork)
             li.innerHTML = (`
-                <p><strong>No account exists with username:</strong> ${username}</p>`);
-            // Append each li to the ul
-            ul.appendChild(li);
-        } else {
-
-            // Get the ul with id of of userRepos
-            let ul = document.getElementById('userRepos');
-            
-            // Loop over each object in data array
-            for (let i in data) {
-                console.log(data[i])
-                if (!(validRepos.includes(data[i].name))) {
-                    continue;
-                }
-
-                // Create variable that will create li's to be added to ul
-                let li = document.createElement('li');
-
-                // Add Bootstrap list item class to each li
-                li.classList.add('pub-entry')
-
-                // Create the html markup for each li
-                tstar = formater('<a class="little-icon" href=' + data[i].html_url + 
-                    '/stargazers><i class="far fa-star"></i>', data[i].stargazers_count, '</a>')
-                tfork = formater('<a class="little-icon" href=' + data[i].html_url + 
-                    '/network/members><i class="fas fa-code-branch"></i>', data[i].forks_count, '</a>')
-                console.log(tfork)
-                li.innerHTML = (`
-                    <p class="no-space"><b><a href="${data[i].html_url}">${data[i].name}</a></b>
-                    <span style="font-size:9.5pt;margin-left:10px;">${data[i].language}` + tstar + tfork + `</span></p>
-                    <p class="no-space-sub" style="padding-top:0px;"> ${data[i].description}
-                    <br><a href="${data[i].html_url}">${data[i].html_url}</a></p>
+                <p class="pub-title"><b><a href="${data[i].html_url}">${data[i].name}</a></b>
+                <span style="font-size:9.5pt;margin-left:10px;">${data[i].language}` + tstar + tfork + `</span></p>
+                <p class="no-space-sub" style="padding-top:0px;"> ${data[i].description}
+                <br><a href="${data[i].html_url}">${data[i].html_url}</a></p>
             `);
 
-                // Append each li to the ul
-                ul.appendChild(li);
-
-            }
+            // Append each li to the ul
+            ul.appendChild(li);
 
         }
     }

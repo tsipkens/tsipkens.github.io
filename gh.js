f57e6b0073e8648eb6f7572b8a7122234ba1653e
tsipkens <tsipkens@gmail.com>
2021-10-21 18:37:30 -0400

    Add header bar
    + Reorg. gh.js to About/Code section to immediately generate.
    + Other minor styling updates.

--- gh.js
+++ gh.js
@@ -8,7 +8,7 @@ for more information.
 requestUserRepos('tsipkens');
 
 
-validRepos = ['atems', 'fmviz', 'cmap', 'mat-2d-aerosol-inversion', 'tfer-pma'];
+validRepos = ['atems', 'fmviz', 'cmap', 'mat-2d-aerosol-inversion', 'tfer-pma', 'odias'];
 
 
 function formater(txt, num, tafter) {

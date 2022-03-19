482223eeed437cfcb5864c116e80fb0e04cab380
Timothy Sipkens <tsipkens@gmail.com>
2022-03-19 01:27:26 -0400

    Add aubos to gh.js

--- js/gh.js
+++ js/gh.js
@@ -8,7 +8,7 @@ for more information.
 requestUserRepos('tsipkens');
 
 
-validRepos = ['atems', 'fmviz', 'cmap', 'mat-2d-aerosol-inversion', 'tfer-pma', 'odias', 'autils'];
+validRepos = ['atems', 'fmviz', 'cmap', 'mat-2d-aerosol-inversion', 'tfer-pma', 'odias', 'autils', 'aubos'];
 
 
 function formater(txt, num, tafter) {

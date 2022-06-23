de93d1f65a28f71632468baf5fb7018d3735d3b7
Timothy Sipkens <tsipkens@uwaterloo.ca>
2022-06-23 16:33:32 -0400

    Sweeping updates to publication lists

--- js/pubs.js
+++ js/pubs.js
@@ -1,20 +1,48 @@
 function formatAuthor(author) {
-  author = author.replace('T. Sipkens', '<u>T. Sipkens</u>').replace('T. A. Sipkens', '<u>T. A. Sipkens</u>');
+  author = author.replace('T. Sipkens', '<b>T. Sipkens</b>').replace('T. A. Sipkens', '<b>T. A. Sipkens</b>');
   return author;
 }
 
+function printYearHeading(data, i, ul, iLastPrinted = i - 1) {
+  if ((i == 0) || (iLastPrinted === null)) {
+    content = data[i].year.toString()
+  } else {
+    if (!(data[iLastPrinted].year == data[i].year)) {
+      content = data[i].year.toString()
+    } else {
+      content = null
+    }
+  }
+  if (!(content === null)) {
+    // Create variable that will create li's to be added to ul
+    let dih = document.createElement('div');
+    dih.setAttribute('data-aos', 'slide-up')
+
+    // Add Bootstrap list item class to each li
+    let h4 = document.createElement('h4');
+    h4.classList.add('h-year')
+
+    // Create the html markup for each li
+    h4.innerHTML = (content);
+
+    // Append each h4 to the ul
+    dih.appendChild(h4);
+    ul.appendChild(dih);
+  }
+}
+
 filterPubs = function (data, st) {
-  
-  if (!(st == null)) {  // skip is search term (st) is empty
-    
+
+  if (!(st == null)) { // skip is search term (st) is empty
+
     st = st.toUpperCase();
-    stq = st.split('"');  // first, split by quotes
-    
-    st = [];  // initialize empty array
+    stq = st.split('"'); // first, split by quotes
+
+    st = []; // initialize empty array
     for (let i in stq) {
-      if ((i % 2) === 0) {  // then split based on spaces
+      if ((i % 2) === 0) { // then split based on spaces
         st = st.concat(stq[i].split(" "));
-      } else {  // then in quotes, add as a block
+      } else { // then in quotes, add as a block
         st.push(stq[i]);
       }
     }
@@ -26,13 +54,13 @@ filterPubs = function (data, st) {
 
     // Filter the data by the search term. 
     data = data.filter(function (entry) {
-      fl = true;  // initialize as true
+      fl = true; // initialize as true
 
       for (let i in st) {
-        sti = st[i];  // part of what makes search case insensitive
-        
+        sti = st[i]; // part of what makes search case insensitive
+
         // Add authors.
-        se = entry.author;  // create text to search in (se)
+        se = entry.author; // create text to search in (se)
         se = se.concat(" ");
 
         // Add entry titles.
@@ -55,15 +83,15 @@ filterPubs = function (data, st) {
           se = se.concat(entry.booktitle);
           se = se.concat(" ");
         }
-        
-        se = se.toUpperCase();  // second part to make case insensitive
+
+        se = se.toUpperCase(); // second part to make case insensitive
 
         // Search. Combine with an "and" to find/exclude all terms.
         if (!(sti.substr(0, 1) === "-")) {
-          fl = fl && se.includes(sti);  // if to include search term
+          fl = fl && se.includes(sti); // if to include search term
         } else {
           sti = sti.substr(1);
-          fl = fl && !(se.includes(sti));  // if to exclude search term
+          fl = fl && !(se.includes(sti)); // if to exclude search term
         }
       }
 
@@ -78,9 +106,12 @@ filterPubs = function (data, st) {
 function writePubs(data, id, yyyy, st = null) {
 
   data = filterPubs(data, st);
+  data = data.sort((a, b) => (b.year - a.year)) // sort entries by year
 
   let ul = document.getElementById(id);
   ul.innerHTML = "";
+
+  var iLastPrinted = null
 
   // Loop over each object in data array
   for (let i in data) {
@@ -92,28 +123,24 @@ function writePubs(data, id, yyyy, st = null) {
       }
     }
 
-    // Create variable that will create li's to be added to ul
-    let di = document.createElement('div');
-    di.setAttribute('data-aos', 'slide-up')
+    // Add year headers.
+    printYearHeading(data, i, ul, iLastPrinted)
+    iLastPrinted = i // copy over current i
 
-    // Add Bootstrap list item class to each li
-    let li = document.createElement('li');
-    li.classList.add('pub-entry')
-
-    content = '<p class="pub-title"><b><a href="' + data[i].doi + '">' + data[i].title + '</a></b></p>';
+    content = '<p class="no-space-sub" style="padding-top:0px;">' + data[i].title + '</b></p>';
     content = content + '<p class="no-space-sub" style="padding-top:0px;"> '
     content = content + formatAuthor(data[i].author);
     content = content + '<span class="no-space-sub">';
-    content = content + ' <br> <i>' + data[i].journal + '</i> (' + data[i].year + ')';
+    content = content + ' <b>&#183</b> <i>' + data[i].journal + '</i> (' + data[i].year + ')';
 
     if (!(data[i].volume === null)) {
-      content = content + ' <b>&#183</b> <b>' + data[i].volume + '</b>, ' + data[i].pages;
+      content = content + ' <b>&#183</b> ' + data[i].volume + ', ' + data[i].pages;
     }
-    
-    if (data[i].doi.includes('arxiv')) {  // for pre-prints
-      content = content + '<br><a href="' + data[i].doi + '">' + data[i].doi + '</a>';
-    } else {  // otherwise for DOIs
-      content = content + '<br><a href="' + data[i].doi + '">' + data[i].doi.replace('https://doi.org/', '') + '</a>';
+
+    if (data[i].doi.includes('arxiv')) { // for pre-prints
+      content = content + ' <b>&#183</b> <a style="display:inline-block;" href="' + data[i].doi + '">' + data[i].doi + '</a>';
+    } else { // otherwise for DOIs
+      content = content + ' <b>&#183</b> <a style="display:inline-block;" href="' + data[i].doi + '">' + data[i].doi.replace('https://doi.org/', '') + '</a>';
     }
 
     if (data[i].hasOwnProperty('field')) {
@@ -125,6 +152,15 @@ function writePubs(data, id, yyyy, st = null) {
     }
 
     content = content + '</span></p>';
+
+
+    // Create variable that will create li's to be added to ul
+    let di = document.createElement('div');
+    di.setAttribute('data-aos', 'slide-up')
+
+    // Add Bootstrap list item class to each li
+    let li = document.createElement('li');
+    li.classList.add('pub-entry')
 
     // Create the html markup for each li
     li.innerHTML = (content);
@@ -138,11 +174,15 @@ function writePubs(data, id, yyyy, st = null) {
 
 
 // For writing HTML from JSON data.
-function writeConf(data, id, type, hon, st = null) {
+function writeConf(data, id, type, hon, st = null, ye = true) {
 
   data = filterPubs(data, st);
+  data = data.sort((a, b) => (b.year - a.year)) // sort entries by year
+
   let ul = document.getElementById(id);
   ul.innerHTML = "";
+
+  var iLastPrinted = null
 
   // Loop over each object in data array
   for (let i in data) {
@@ -164,6 +204,12 @@ function writeConf(data, id, type, hon, st = null) {
       }
     }
 
+    // Add year headers.
+    if (ye) {
+      printYearHeading(data, i, ul, iLastPrinted)
+    }
+    iLastPrinted = i // copy over current i
+
     // Create variable that will create li's to be added to ul
     let di = document.createElement('div');
     di.setAttribute('data-aos', 'slide-up')
@@ -172,7 +218,7 @@ function writeConf(data, id, type, hon, st = null) {
     let li = document.createElement('li');
     li.classList.add('pub-entry')
 
-    content = '<p class="pub-title"><b><a href="' + data[i].doi + '">' + data[i].title + '</a></b></p>';
+    content = '<p class="no-space-sub" style="padding-top:0px;">' + data[i].title + '</p>';
     content = content + '<p class="no-space-sub" style="padding-top:0px;"> '
     content = content + formatAuthor(data[i].author);
     content = content + '<span class="no-space-sub">';
@@ -180,7 +226,7 @@ function writeConf(data, id, type, hon, st = null) {
     content = content + ' <b>&#183</b> ' + data[i].date + ', ' + data[i].year;
 
     content = content + '</span></p>';
-    
+
     if ((data[i].hasOwnProperty('honours')) || (data[i].hasOwnProperty('PDF'))) {
       content = content + '<p class="pub-title">';
 
@@ -191,7 +237,7 @@ function writeConf(data, id, type, hon, st = null) {
           content = content + data[i].honours + '</span> ';
         }
       }
-      
+
       if (data[i].hasOwnProperty('PDF')) {
         content = content + '<span class="pub-honour">';
         content = content + '<a href="' + data[i].PDF + '" style="text-decoration:none;">';
